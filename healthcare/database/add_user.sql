2ee0fd30bb7214469f3de7757e3a75a557218037
Nathan Diedrick <nathan.diedrick@gmail.com>
2021-02-21 19:18:59 -0600

    fixed add_user

--- healthcare/database/add_user.sql
+++ healthcare/database/add_user.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user(
     user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(63) NOT NULL,
+    username VARCHAR(63) UNIQUE NOT NULL,
     password VARCHAR(512) NOT NULL,
     first_name VARCHAR(63) NOT NULL,
     last_name VARCHAR(63) NOT NULL,
     user_type INT NOT NULL,
-    user_status INT NOT NULL,
+    user_status INT NOT NULL
 );
