d5d1330c546320873ebf868d53d0f74962ce1c12
saheethao <thao.sahee@uwlax.edu>
2021-04-19 13:33:14 -0500

    drugs drugs drugs drugs

---
+++ insert_data/insurance_drug.sql
@@ -0,0 +1,7 @@
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Levothyroxine', 'Levoxyl', '0781-5180-10');
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Lisinopril', 'Zestril', '0781-5180-11');
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Felix Felicis', 'Likluk', '0781-5180-12');
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Atorvastatin', 'Lipitor', '0781-5180-13' );
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Melange', 'Spycen', '0781-5180-14');
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Soma', 'Huxley', '0781-5180-15');
+INSERT INTO drug ( drug_name, commercial_name, drug_code ) VALUES ( 'Amoxicillin', 'Moxie', '0781-5180-16');
